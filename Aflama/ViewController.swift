fc39fa8a1cf8ddc273682b692f63c3f8d77e6959
esslam emad <esslam69@gmail.com>
2019-02-24 12:53:13 +0200

    Initial Commit

---
+++ Aflama/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Aflama
+//
+//  Created by Esslam Emad on 24/2/19.
+//  Copyright © 2019 Alyom Apps. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
